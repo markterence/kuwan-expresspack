2a0fecc6aba95e57599897081e5b9a78e0595c31
Mark Terence Tiglao <github@markterence.me>
2025-07-17 12:22:19 +0800

    chore: untrack output / dist from playground

--- playground/.output/app.js
+++
@@ -1,9 +0,0 @@
-// app.ts
-import { createApp } from "@markterence/kuwan-expresspack";
-var app = createApp();
-app.get("/", (req, res) => {
-  res.send("Hello from playground app!");
-});
-app.listen(3000, () => {
-  console.log("Server is running");
-});
