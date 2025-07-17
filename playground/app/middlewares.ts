d9fb153578c46197d242ee81cd75e73b6307b589
Mark Terence Tiglao <github@markterence.me>
2025-07-17 19:55:32 +0800

    chore: update playground

--- playground/app/middlewares.ts
+++ playground/app/middlewares.ts
@@ -8,4 +8,18 @@ import {
 export default defineMiddlewares(({ app, router }) => {
     app.use(jsonBodyParser())
     app.use(urlencodedBodyParser())
+
+    router.get('/middlewares', (req, res) => {
+        res.json({
+            message: 'Middlewares are defined and working!',
+        });
+    });
+
+    router.post('/middlewares', (req, res) => {
+        const data = req.body;
+        res.json({
+            message: 'Data received via middlewares',
+            data,
+        });
+    })
 })
