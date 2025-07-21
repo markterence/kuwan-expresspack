df646426a375e0b9a993d88c95099fb8db83f005
Mark Terence Tiglao <github@markterence.me>
2025-07-22 02:22:12 +0800

    refactor: update resolveAppPaths to correct config path and improve file extension handling

--- packages/kuwan-expresspack/src/lib/resolveAppPaths.ts
+++ packages/kuwan-expresspack/src/lib/resolveAppPaths.ts
@@ -3,10 +3,11 @@ import path, { join } from 'node:path'
 
 export function resolveAppPaths(root: string) {
     const app = join(root, 'app')
-    // Subdirectories
-    const config = join(app, 'config') 
+
+    const config = join(root, 'config') 
     const bodyParserConfig = join(config, 'body-parser')
 
+    // Subdirectories of `app` directory
     const controllers = join(app, 'controllers')
     const services = join(app, 'services')
     const errors = join(app, 'errors')
@@ -14,8 +15,6 @@ export function resolveAppPaths(root: string) {
     // Main application directory 
     const middlewareFilePath = join(app, 'middlewares')
     const routesFilePath = join(app, 'routes')
-
-
 
     return {
         root,
@@ -47,9 +46,31 @@ export function resolveAppPaths(root: string) {
  * Also, when bundled, the file extension will become `.js` or `.mjs` and not `.ts`.
  */
 export const getFileCandidate = (filePath: string) => {
-    const candidates = [`${filePath}.ts`, `${filePath}.js`, `${filePath}.mjs`];
-    const file = candidates.find((p) => existsSync(p));
-    return file;
+    // Old implementation that checks for multiple extensions.
+    // const candidates = [`${filePath}.ts`, `${filePath}.js`, `${filePath}.mjs`];
+    // const file = candidates.find((p) => existsSync(p));
+    // return file;
+
+    // New implementation that checks for multiple extensions.
+    // with ordered priority.
+    // Priority order: .ts -> .mts -> .js -> .mjs -> .cjs -> .cts
+    const extensions = ['ts', 'mts', 'js', 'mjs', 'cjs', 'cts']
+    
+    for (const ext of extensions) {
+        const file = `${filePath}.${ext}`;
+        if (existsSync(file)) {
+            return file
+        }
+    }
+    
+    return null
+}
+
+export const checkDir = (filePath: string) => { 
+    if (existsSync(filePath)) {
+        return filePath
+    } 
+    return null
 }
 
 export const getRelativeFilePath = (filePath: string, root: string) => {
