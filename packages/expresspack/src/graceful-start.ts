277b98cae794ed2a3e28b79ed72bc709fcbd7b71
Mark Terence Tiglao <github@markterence.me>
2025-08-02 16:40:49 +0800

    fix: update event emitter import path to use the correct service

--- packages/expresspack/src/graceful-start.ts
+++ packages/expresspack/src/graceful-start.ts
@@ -1,7 +1,7 @@
 import type { Express } from 'express';
 import logger from './logger'
 import type { Server } from 'node:http'; 
-import emitter from './services/emitter';
+import emitter from './services/event';
 
 export function gracefulHTTPStart(app: Express, port: number, onStart?: () => Promise<void>): Server {
     const server = app.listen(port, async () => {
