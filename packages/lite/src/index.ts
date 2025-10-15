4410d577f268b94fe3541c98b795e1fd63e3b3fc
Mark Terence Tiglao <github@markterence.me>
2025-10-15 16:05:08 +0800

    fix: export types

--- packages/lite/src/index.ts
+++ packages/lite/src/index.ts
@@ -2,7 +2,12 @@
 export { gracefulHTTPStart } from './graceful-start';
 export { gracefulShutdown } from './graceful-shutdown';
 export { defineKernel } from './define-kernel';
-export { loadKernel } from './load-kernel';
+export type { AppContext, KernelExports } from './define-kernel';
+export { loadKernel} from './load-kernel';
+export type { 
+    AppBootstrapContext,
+    LoadKernelOptions,
+} from './load-kernel';
 export { createRouter } from './router-factory';
 export { defineRouterMiddleware } from './define-router-middleware';
 export { configLoader } from './config-loader';
