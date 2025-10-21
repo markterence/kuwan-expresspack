f74c8afd0561959b5b65640517b5ab52cb8260e8
Mark Terence Tiglao <github@markterence.me>
2025-10-22 01:27:20 +0800

    feat(logger): add function to create logger specific log-level

--- packages/lite/src/services/logger/index.ts
+++ packages/lite/src/services/logger/index.ts
@@ -1,4 +1,4 @@
-import consola, { type ConsolaInstance } from 'consola';
+import consola, { type ConsolaInstance, type ConsolaOptions } from 'consola';
 import { env } from 'node:process';
 const logLevel = env.APP_LOG_LEVEL;
 
@@ -6,6 +6,19 @@ export const println = consola.create({
     level: Number.isNaN(Number(logLevel)) ? 3 : Number(logLevel) 
 })
 
+/**
+ * Create a console logger with a specific name. Uses the log-level specified in `APP_LOG_LEVEL` env variable.
+ */
 export function useConsoleLogger(name: string): ConsolaInstance {
     return println.withTag(name);
+}
+
+/**
+ * Create a console logger allowing to specify log level in options.
+ */
+export function createConsoleLogger(name: string, options: Partial<Pick<ConsolaOptions, "level">>): ConsolaInstance {
+    const logger = consola.create({
+        level: options.level ?? (Number.isNaN(Number(logLevel)) ? 3 : Number(logLevel))
+    })
+    return logger.withTag(name);
 }
