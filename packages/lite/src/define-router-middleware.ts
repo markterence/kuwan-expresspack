fa30b1dc618d712a1465854ed777b840ae37caba
Mark Terence Tiglao <github@markterence.me>
2026-05-07 13:57:12 +0800

    feat(router): enhance defineRouterMiddleware to accept app context and improve type safety

--- packages/lite/src/define-router-middleware.ts
+++ packages/lite/src/define-router-middleware.ts
@@ -1,13 +1,48 @@
 import { createRouter } from './router-factory';
-import type { Router, RouterOptions } from 'express';
+import { Router, type RouterOptions, type Express } from 'express';
+
+export interface DefineRouteMiddlewareContext {
+    // Making it optional for now, so it wont break
+    // existing uses of `defineRouterMiddleware`.
+    app?: Express,
+    router: Router
+}
+
+export interface DefineRouterMiddlewareReturn {
+    (app?: Express): Router
+}
 
 /**
  * Utility to define a router middleware using a callback.
  * @param fn Callback to register routes on the router
  * @returns Express Router instance
+ * 
+ * @example 
+ * 
+ * ```ts
+ * const routerMiddleware =  defineRouterMiddleware(({ router, app }) => {
+ *  router.get('/hello', (req, res) => {
+ *    res.send('Hello from router middleware!');
+ *  });
+ *  
+ *  app.use(controller);
+ * });
+ * 
+ * // In your main app file
+ * const app = express();
+ * app.use(middlewares);
+ * routerMiddleware(app);
  */
-export function defineRouterMiddleware(fn: (r: Router) => void, options?: RouterOptions): Router {
-    const r = createRouter(options);
-    fn(r);
-    return r;
+export function defineRouterMiddleware(
+    fn: (context: DefineRouteMiddlewareContext) => void, 
+    options?: RouterOptions
+): DefineRouterMiddlewareReturn {
+    // const r = createRouter(options);
+    // fn(r);
+    // return r;
+    return (app?: Express) =>{
+        const r = createRouter(options);
+        fn({ app, router: r });
+        return r;
+    };
 }
