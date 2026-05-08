87eb5346e4db67d144bb8b513bf6193022f0ddd1
Mark Terence Tiglao <github@markterence.me>
2026-05-08 14:23:15 +0800

    feat(router): add option for defineRouterMiddleware to support internal router mounting
    Option `mountInternalRouter` was added
    to also register the router using `app.use(router)`

--- packages/lite/src/define-router-middleware.ts
+++ packages/lite/src/define-router-middleware.ts
@@ -12,6 +12,14 @@ export interface DefineRouterMiddlewareReturn {
     (app?: Express): Router
 }
 
+export interface DefineRouterMiddlewareOptions extends RouterOptions {
+    /**
+     * When `true`, the internal router will be mounted on the app so it  
+     * can be used directly in the callback.
+     */
+    mountInternalRouter?: boolean;
+}
+
 /**
  * Utility to define a router middleware using a callback.
  * @param fn Callback to register routes on the router
@@ -40,9 +48,33 @@ export function defineRouterMiddleware(
     // const r = createRouter(options);
     // fn(r);
     // return r;
+
+    const defaultOptions: DefineRouterMiddlewareOptions = {
+        mountInternalRouter: false
+    };
+
+    const isExpressInstance = (app: any): app is Express => {
+        return app && typeof app.use === 'function' && typeof app.get === 'function';
+    }
+
+    // extract original Express RouterOptions
+    const { mountInternalRouter, ...routerOptions } = { ...defaultOptions, ...options };
+
     return (app?: Express) =>{
-        const r = createRouter(options);
+        const r = createRouter(routerOptions);
         fn({ app, router: r });
+        
+        if (mountInternalRouter) {
+            // check if app is Express instance and if so, we 
+            // need to register the router with the app. 
+            // Usage is `routerMiddleware(app)`, but since 
+            // `router` is passed to the callback, when user uses the router callback
+            // like `router.get()`, it will not be registered with the app, so we need to do it here
+            if (isExpressInstance(app)) {
+                app.use(r);
+            }
+        }
+
         return r;
     };
 }
